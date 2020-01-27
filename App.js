664573473862048ca3df0069aefcea10f78835f2
Remarc Balisi <remarc.balisi@fligno.com>
2020-01-27 14:46:06 +0800

    use react component

--- App.js
+++ App.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import 'react-native-gesture-handler';
-import HomeScreen from './src/tabs/HomeScreen';
-
-// class HomeScreen extends React.Component {
-//   render() {
-//     return (
-//       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-//         <Text>Home!</Text>
-//       </View>
-//     );
-//   }
-// }
-
-class SettingsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from './src/screens/HomeScreen';
+import MessagesScreen from './src/screens/MessagesScreen';
 
 const TabNavigator = createBottomTabNavigator({
   Home: HomeScreen,
-  Settings: SettingsScreen,
+  Messages: MessagesScreen,
 });
 
 export default createAppContainer(TabNavigator);
---
+++ src/screens/MessagesScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class MessagesScreen extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Messages Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default MessagesScreen;
