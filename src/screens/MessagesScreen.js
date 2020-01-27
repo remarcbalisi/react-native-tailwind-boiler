863cc8543ab4647e0d87b28127b19c51a2514dfd
Remarc Balisi <remarc.balisi@fligno.com>
2020-01-27 14:52:13 +0800

    functional screens

--- src/screens/MessagesScreen.js
+++ src/screens/MessagesScreen.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Text, View} from 'react-native';
 
-class MessagesScreen extends Component {
-  render() {
-    return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Messages Screen</Text>
-      </View>
-    );
-  }
-}
+const MessagesScreen = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Messages Screen</Text>
+    </View>
+  );
+};
 
 export default MessagesScreen;
